61331f4f601f7098ab9027eca163b931bdafb0af
Asiah Clark <clarkaa0@gmail.com>
2024-02-26 18:34:34 -0600

    Added cap on how many guesses the user can take before revealing the correct answer

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,10 @@
 number = 10
 
 print("I'm thinking of a number...")
-while True:
+counter = 3
+while counter > 0:
     guess = input("What number am I thinking of? ")
+    counter -= 1
 
     if guess == 'q':
         print(f"The number was {number}.")
@@ -10,7 +12,10 @@ while True:
     elif int(guess) == number:
         print("Congratulations! You guessed the right number.")
         break
+    elif int(guess) > number:
+        print(f"You guessed too high. \nYou now have {counter} guesses left.")
     else:
-        print("Sorry! That was incorrect try again. ")
-
+        print(f"You guessed too low. \nYou now have {counter} guesses left.")
+    
+print(f"The number was {number}")
     
